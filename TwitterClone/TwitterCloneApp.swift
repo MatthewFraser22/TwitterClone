f9e57abe2d2be4fe8e30b5a182a062258d5f6179
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2022-12-12 14:49:20 -0500

    initial code commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Matthew Fraser on 2022-12-08.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
