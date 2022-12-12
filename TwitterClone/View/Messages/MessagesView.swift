6adcb433b3e92380db7f0d7bcf142711f054032d
Matthew Fraser <52222907+MatthewFraser22@users.noreply.github.com>
2022-12-12 15:05:38 -0500

    Implement TabBar

---
+++ TwitterClone/View/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  Messages.swift
+//  TwitterClone
+//
+//  Created by Matthew Fraser on 2022-12-12.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Messages_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
